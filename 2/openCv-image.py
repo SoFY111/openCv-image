a771c294e8ae5b8513f98fb169b9594c21d71e40
Hakan DİNÇTÜRK <hakan.dincturkk@gmail.com>
2021-04-27 19:42:57 +0300

    multiProcess

--- 2/openCv-image.py
+++ 2/openCv-image.py
@@ -2,10 +2,11 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-img = cv2.imread('img/mona2.jpg', 0)
+img = cv2.imread('img/ben2.png', 0)
 img2 = img.copy()
-template = cv2.imread('img/mona1.jpg', 0)
+template = cv2.imread('img/ben1.jpg', 0)
 w, h = template.shape[::-1]
+
 
 # All the 6 methods for comparison in a list
 methods = ['cv2.TM_CCOEFF', 'cv2.TM_CCOEFF_NORMED', 'cv2.TM_CCORR',
